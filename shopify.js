937e8a72c75b4b6cfddd54a610d815feb469a5c3
Asfar Anjum <asfarpro@Asfars-MacBook-Pro.local>
2025-08-16 02:20:16 +0500

    ✅ Updhfgated billing redirect logic

--- shopify.js
+++ shopify.js
@@ -1,9 +1,11 @@
 import { shopifyApi } from '@shopify/shopify-api';
+import { shopifyApp } from '@shopify/shopify-api/adapters/node';
 
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   hostName: process.env.HOST.replace(/^https?:\/\//, ''),
-  apiVersion: '2024-07', // explicitly use a valid version string
+  apiVersion: '2024-07',
   isEmbeddedApp: true,
+  adapter: shopifyApp(),
 });
