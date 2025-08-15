08621f8767165cc5dfc0c65651c27e2cc90a9c52
Asfar Anjum <asfarpro@Asfars-MacBook-Pro.local>
2025-08-16 02:10:16 +0500

    ✅ Updzcxzated billing redirect logic

--- shopify.js
+++ shopify.js
@@ -1,9 +1,9 @@
-import { shopifyApi } from '@shopify/shopify-api';
+import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
 
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   hostName: process.env.HOST.replace(/^https?:\/\//, ''),
-  apiVersion: '2024-07',
+  apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: true,
 });
