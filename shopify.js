c8423a76abad13c0ebd196ee8d2d254a407f4a42
Asfar Anjum <asfarpro@Asfars-MacBook-Pro.local>
2025-08-16 02:22:54 +0500

    Fix shopify.js to remove v12 adapter usage

--- shopify.js
+++ shopify.js
@@ -1,11 +1,10 @@
 import { shopifyApi } from '@shopify/shopify-api';
-import { shopifyApp } from '@shopify/shopify-api/adapters/node';
 
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   hostName: process.env.HOST.replace(/^https?:\/\//, ''),
   apiVersion: '2024-07',
-  isEmbeddedApp: true,
-  adapter: shopifyApp(),
+  isEmbeddedApp: true
 });
+
